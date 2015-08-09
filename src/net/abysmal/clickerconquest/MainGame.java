0d86e1da5ef253e93d3ed870585da12eb1033c9e
Mojken Moremeril <Mikaelgillis@gmail.com>
2015-08-09 18:58:56 +0200

    Temp.JFrame for main game

---
+++ src/net/abysmal/clickerconquest/MainGame.java
@@ -0,0 +1,35 @@
+package net.abysmal.clickerconquest;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class MainGame extends JFrame {
+
+	private JPanel contentPane;
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+
+			public void run() {
+				try{
+					MainGame frame = new MainGame();
+					frame.setVisible(true);
+				}catch (Exception e){
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	public MainGame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+}
